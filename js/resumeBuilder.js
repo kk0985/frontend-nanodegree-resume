84c409f27f65b5191b057ec6609b96c37224e1a0
Kris Klingberg <kk0985@gmail.com>
2015-08-09 13:43:37 -0400

    Add var Bio appended/prepended as needed

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,18 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+	"Name" : "Kris Klingberg",
+	"Role" : "Web Developer",
+	"Contacts" : {
+		"mobile" : "678-427-1056",
+	}
+};
+
+var formattedName = HTMLheaderName.replace("%data%", bio.Name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.Role);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+
+var formattedmobile = HTMLmobile.replace("%data%", bio.Contacts.mobile);
+$("#topContacts").append(formattedmobile);
+
+$("#footerContacts").append(formattedmobile);
